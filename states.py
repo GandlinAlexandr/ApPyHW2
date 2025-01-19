6b12e0c61681dcb2ee70e53a52554dd0a83bd659
Gandlin Alexander <137958267+GandlinAlexandr@users.noreply.github.com>
2025-01-19 04:36:03 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,5 +1,6 @@
 from aiogram.fsm.state import State, StatesGroup
 
+
 # Состояние для формы анкеты
 class ProfileSetup(StatesGroup):
     weight = State()
@@ -9,10 +10,12 @@ class ProfileSetup(StatesGroup):
     city = State()
     calorie_goal = State()
 
+
 # Состояние для формы пищи
 class Food(StatesGroup):
     eat_weight = State()
     calories_per_100_g = State()
+
 
 class ProfileChange(StatesGroup):
     value = State()
