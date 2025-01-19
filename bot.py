eb3a6dcd0ef412cdfb5d0b20a0d0dfc52509b3ed
Gandlin Alexander <137958267+GandlinAlexandr@users.noreply.github.com>
2025-01-19 04:34:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,11 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 dp.include_router(router)
 
+
 async def main():
     print("Бот запущен!")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
